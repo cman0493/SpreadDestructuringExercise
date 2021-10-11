f0e5b2401eee557fbc2e652da1daa7a91f6a2923
Chris Hunley <cshunley21@gmail.com>
2021-10-11 09:57:08 -0500

    fixed some errors

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 //1
 const mcuShows = ["Loki", "What If...?"];
 const starWarsShows = ["The Mandalorian", "The Bad Batch"];
-const disneyPlusShows = [mcuShows, starWarsShows, "Monsters At Work"]
+const disneyPlusShows = [...mcuShows, ...starWarsShows, "Monsters At Work"]
 console.log(disneyPlusShows);
 
 //2
-const netflixMovies = {action: "The Tomorrow War", drama: "One Night In Miami"}
-const amazonPrimeMovies = {musical: "Hamilton"}
-const streamingMovies = {...netflixMovies, ...amazonPrimeMovies}
+const netflixMovies = {action: "Extraction", crime: "The Irishman"};
+const amazonPrimeMovies = {action: "The Tomorrow War", drama: "One Night In Miami"};
+const streamingMovies = {...amazonPrimeMovies, ...netflixMovies, musical: "Hamilton"};
 console.log(streamingMovies);
 
 //3
 const disneyJunior = ["Mickey Mouse Clubhouse", "Spidey and His Amazing Friends"];
-const junior = {mickey: "Mickey Mouse Clubhouse", spidey: "Spidey and His Amazing Friends"};
-console.log(junior);
+const [mickey, spidey] = disneyJunior;
+console.log(disneyJunior);
 
 //4
 const avengers = {warMachine: "James Rhodes", theHulk: "Bruce Banner"};
-let {James Rhodes = "warMachine"}
-let {Bruce Banner = "theHulk"};
+const {warMachine, theHulk} = avengers;
 console.log(avengers);
 const moreAvengers = {blackWidow: "Natasha Romanoff", hawkeye: "Clint Barton", ironMan: "Tony Stark"};
-let nat = "Natasha Romanoff"
-let others = Clint Barton", "Tony Stark"
-let {nat, others} = moreAvengers
+const {blackWidow: nat, ...other} = moreAvengers;
+console.log(nat, other);
 
 //5
 const bonus = {
@@ -31,13 +29,8 @@ const bonus = {
     second: [4, 5, 6],
     third: [7, 8, 9]
   };
-const all = {...first, ...second, ...third}
-let {const one = 1}
-let {const two = 2}
-let {const three = 3}
-let {const four = 4}
-let {const five = 5}
-let {const six = 6}
-let {const seven = 7}
-let {const eight = 8}
-let {const nine = 9}
+  const all = [...bonus.first, ...bonus.second, ...bonus.third];
+  const {first, second, third} = bonus;
+  const [one, two, three] = first;
+  const [four, five, six] = second;
+  const [seven, eight, nine] = third;
